2edb841052ddf635ef3d1210976df4418c5a4f09
xkalkas <xkalkas@gmail.com>
2025-11-03 11:30:26 +0200

    Add eventlisteners for onscreen btns and adjust
    Adjust logic for ui input instead of prompts

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ function operate(num1, num2, operator){
 
 function add(num1, num2){
     let result = (+num1)+(+num2);
-    return Math.round(result * 100) / 100;
+    return Math.round(result * 1000) / 1000;
 }
 
 function subtract(num1, num2){
     let result = (+num1)-(+num2);
-    return Math.round(result * 100) / 100;
+    return Math.round(result * 1000) / 1000;
 }
 
 function multiply(num1, num2){
     let result = (+num1)*(+num2);
-    return Math.round(result * 100) / 100;
+    return Math.round(result * 1000) / 1000;
 }
 
 function divide(num1, num2){
@@ -25,5 +25,57 @@ function divide(num1, num2){
         return 'Number 2 cannot be 0';
     }
     let result = (+num1)/(+num2);
-    return Math.round(result * 100) / 100;
+    return Math.round(result * 1000) / 1000;
 }
+
+
+let input = document.querySelector('input');
+let operator = "";
+let currentNum = "";
+let previousNum = 0;
+let result = 0;
+let equals = false;
+
+document.addEventListener("click", event => {
+    let btnClicked = event.target;
+    
+    if (btnClicked.classList.contains('operator')){
+        operator = btnClicked.textContent;
+        // if(currentNum!="" && previousNum!=0){
+        //     result = operate(previousNum, currentNum, operator)
+        //     equals = true;
+        //     currentNum = result;
+        // }
+        previousNum = +currentNum;
+        currentNum = "";
+        console.log(operator);
+        
+    }
+    if (btnClicked.classList.contains('number')){
+        if(equals){
+            currentNum = btnClicked.textContent;
+            equals = false;
+            console.log(currentNum);
+        }else{
+            currentNum = currentNum + btnClicked.textContent;
+            console.log(currentNum);
+        }
+    }
+    if (btnClicked.classList.contains('equals')){
+        result = operate(previousNum, currentNum, operator)
+        equals = true;
+        currentNum = result;
+        console.log(result)
+    }
+    if (btnClicked.classList.contains('clear')){
+        operator = ""
+        currentNum = "";
+        previousNum = 0;
+        result = 0
+        equals = false;
+        console.clear();
+    }
+    if (btnClicked.classList.contains('backspace')){
+        currentNum = "";
+    }
+});
