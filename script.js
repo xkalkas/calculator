7df578604959c5c454034e6fb9b9f58ca2de06f6
xkalkas <xkalkas@gmail.com>
2025-11-04 09:01:19 +0200

    Add support for stringing operators and display

--- script.js
+++ script.js
@@ -28,44 +28,53 @@ function divide(num1, num2){
     return Math.round(result * 1000) / 1000;
 }
 
+function displayResult(){
+
+}
+
 
 let input = document.querySelector('input');
 let operator = "";
 let currentNum = "";
 let previousNum = 0;
 let result = 0;
-let equals = false;
+let equals = false; // check if you have clicked equals and then a number before an operator to clear the numbers
+let stringOperators = false; // check if you string operators before clicking equals to display the result
 
 document.addEventListener("click", event => {
     let btnClicked = event.target;
     
     if (btnClicked.classList.contains('operator')){
+        if(stringOperators){  
+            result = operate(previousNum, currentNum, operator)
+            previousNum = result;
+            currentNum = "";
+            input.value = result;
+        }
         operator = btnClicked.textContent;
-        // if(currentNum!="" && previousNum!=0){
-        //     result = operate(previousNum, currentNum, operator)
-        //     equals = true;
-        //     currentNum = result;
-        // }
-        previousNum = +currentNum;
-        currentNum = "";
-        console.log(operator);
-        
+        if(!stringOperators){
+            stringOperators = true;
+            previousNum = +currentNum;
+            currentNum = "";
+        }   
     }
+
     if (btnClicked.classList.contains('number')){
         if(equals){
             currentNum = btnClicked.textContent;
             equals = false;
-            console.log(currentNum);
+            input.value = currentNum;
         }else{
             currentNum = currentNum + btnClicked.textContent;
-            console.log(currentNum);
+            input.value = currentNum;
         }
     }
     if (btnClicked.classList.contains('equals')){
         result = operate(previousNum, currentNum, operator)
         equals = true;
         currentNum = result;
-        console.log(result)
+        stringOperators = false;
+        input.value = result;
     }
     if (btnClicked.classList.contains('clear')){
         operator = ""
@@ -73,9 +82,61 @@ document.addEventListener("click", event => {
         previousNum = 0;
         result = 0
         equals = false;
-        console.clear();
+        stringOperators = false;
+        input.value = "";
     }
     if (btnClicked.classList.contains('backspace')){
+        input.value = "";        
         currentNum = "";
     }
 });
+
+
+
+// document.addEventListener('keydown', event => {
+//     if (event.key){
+//         if(stringOperators){  
+//             result = operate(previousNum, currentNum, operator)
+//             previousNum = result;
+//             currentNum = "";
+//             input.value = result;
+//         }
+//         operator = btnClicked.textContent;
+//         if(!stringOperators){
+//             stringOperators = true;
+//             previousNum = +currentNum;
+//             currentNum = "";
+//         }   
+//     }
+
+//     if (btnClicked.classList.contains('number')){
+//         if(equals){
+//             currentNum = btnClicked.textContent;
+//             equals = false;
+//             input.value = currentNum;
+//         }else{
+//             currentNum = currentNum + btnClicked.textContent;
+//             input.value = currentNum;
+//         }
+//     }
+//     if (btnClicked.classList.contains('equals')){
+//         result = operate(previousNum, currentNum, operator)
+//         equals = true;
+//         currentNum = result;
+//         stringOperators = false;
+//         input.value = result;
+//     }
+//     if (btnClicked.classList.contains('clear')){
+//         operator = ""
+//         currentNum = "";
+//         previousNum = 0;
+//         result = 0
+//         equals = false;
+//         stringOperators = false;
+//         input.value = "";
+//     }
+//     if (btnClicked.classList.contains('backspace')){
+//         input.value = "";        
+//         currentNum = "";
+//     }
+// });
